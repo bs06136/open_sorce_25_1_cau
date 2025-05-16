9074141b6b55de03e23366a3f4afd1924bfe859a
rlaxodn0320 <48887569+rlaxodn0320@users.noreply.github.com>
2025-05-17 02:14:20 +0900

    UnityBaseSystemUI

---
+++ Assets/Script/TowerLoading.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+public class TowerLoading : MonoBehaviour
+{
+    public GameObject objectToActivate;
+    public GameObject objectToActivate2;
+    public GameObject objectToDeactivate;
+    public float delay = 3f;
+
+    void Start()
+    {
+        StartCoroutine(SwitchObjectsAfterDelay());
+    }
+
+    IEnumerator SwitchObjectsAfterDelay()
+    {
+        yield return new WaitForSeconds(delay);
+
+        if (objectToDeactivate != null)
+            objectToDeactivate.SetActive(false);
+
+        if (objectToActivate != null)
+            objectToActivate.SetActive(true);
+
+        if (objectToActivate2 != null)
+            objectToActivate2.SetActive(true);
+    }
+}
