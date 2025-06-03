4db1537c85eb8d79f181caf34cac01b5a084ce93
Eojin <eojindr@gmail.com>
2025-06-03 16:25:07 +0900

    게임 종료 및 재시작

--- Assets/Script/Manager/GameOverHandler.cs
+++ Assets/Script/Manager/GameOverHandler.cs
@@ -13,37 +13,9 @@ public static class GameOverHandler
             UnityEngine.Object.Destroy(GameManager.Instance.gameObject);
         }
 
-        // 캔버스 전환
-        var gameOverCanvas = GameObject.Find("GameOverCanvas");
-        if (gameOverCanvas == null)
-        {
-            Debug.LogError("GameOverCanvas 오브젝트를 찾을 수 없습니다!");
-        }
-        else
-        {
-            Debug.Log("GameOverCanvas 오브젝트를 성공적으로 찾음");
-        }
-        var mainMenuCanvas = GameObject.Find("MainMenu");
-        var ingameCanvas = GameObject.Find("InGame");
 
-        if (ingameCanvas == null)
-        {
-            Debug.LogError("InGame 오브젝트를 찾을 수 없습니다!");
-        }
-        else
-        {
-            Debug.Log("InGame 오브젝트를 성공적으로 찾음");
-            ingameCanvas.SetActive(false);
-        }
-
-        if (gameOverCanvas != null)
-            gameOverCanvas.SetActive(true);
-        Debug.Log($"GameOverCanvas 활성화 상태: {gameOverCanvas.activeSelf}");
-        if (mainMenuCanvas != null)
-            mainMenuCanvas.SetActive(false);
-        if (ingameCanvas != null)
-            ingameCanvas.SetActive(false);
-
+        GameManager.Instance.ShowGameOver();
         Debug.Log("게임 오버: 캔버스 전환");
     }
+
 }
--- Assets/Script/UI/Return_to_main.cs
+++ Assets/Script/UI/Return_to_main.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Return_to_main : MonoBehaviour
 {
     public GameObject MainMenuCanvas;
     public GameObject GameOverCanvas;
 
-    public void ShowMainMenu()
+    public void backToMainMenu()
     {
-        MainMenuCanvas.SetActive(true);
-        GameOverCanvas.SetActive(false);
+        //     MainMenuCanvas.SetActive(true);
+        //     GameOverCanvas.SetActive(false);
+        // }
+
+        // public void RetryGame()
+        // {
+        SceneManager.LoadScene("SampleScene");
     }
 }
