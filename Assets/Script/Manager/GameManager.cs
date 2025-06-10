904cc110fe6511c994ecfa6e43e6b820bea7be43
Eojin <eojindr@gmail.com>
2025-06-11 03:23:20 +0900

    CharacterUI

--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -42,6 +42,8 @@ public class GameManager : MonoBehaviour
     public GameObject settingCanvas;
     public GameObject gameClearCanvas;
     public GameObject ingameSettingCanvas;
+    public GameObject storyCanvas;
+    public GameObject CharacterCanvas;
 
     [Header("Victory UI")]
     public GameObject victoryPanel;
@@ -72,6 +74,8 @@ public class GameManager : MonoBehaviour
         settingCanvas.SetActive(false);
         gameClearCanvas.SetActive(false);
         ingameSettingCanvas.SetActive(false);
+        storyCanvas.SetActive(false);
+        CharacterCanvas.SetActive(false);
 
         GameEvents.OnCardStatusRequested = GetCardStatus;
         GameEvents.OnCardChosen = ApplyCardByIndex;
